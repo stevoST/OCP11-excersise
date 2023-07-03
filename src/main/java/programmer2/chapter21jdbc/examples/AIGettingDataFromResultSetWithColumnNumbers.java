2fd13e98eb33a0da63c19edeb9148f8ccb8e5c8c
Stefan Starosta <stefan.starosta@gmail.com>
2023-07-03 08:12:03 +0200

    chapter 21 getting data from resultset

---
+++ src/main/java/programmer2/chapter21jdbc/examples/AIGettingDataFromResultSetWithColumnNumbers.java
@@ -0,0 +1,33 @@
+package programmer2.chapter21jdbc.examples;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AIGettingDataFromResultSetWithColumnNumbers {
+    public static void main(String[] args) throws SQLException {
+
+        // add derby.jar to class path
+
+        Connection conn = DriverManager.getConnection("jdbc:derby:c:\\tools\\db-derby\\zoo;create=true;");
+
+        String sql = "SELECT id, name FROM exhibits";
+        Map<Integer,String> idToNameMap = new HashMap<>();
+
+        try (var ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()){
+
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                idToNameMap.put(id, name);
+            }
+
+            System.out.println( idToNameMap);
+
+        }
+    }
+}
