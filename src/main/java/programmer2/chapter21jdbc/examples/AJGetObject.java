e7ff666d1f82be9b3833ed319fbb8a82bca78b07
Stefan Starosta <stefan.starosta@gmail.com>
2023-07-04 07:02:07 +0200

    chapter 21 tested getObject

---
+++ src/main/java/programmer2/chapter21jdbc/examples/AJGetObject.java
@@ -0,0 +1,35 @@
+package programmer2.chapter21jdbc.examples;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AJGetObject {
+
+    // add derby.jar to class path
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:derby:c:\\tools\\db-derby\\zoo;create=true;");
+
+        String sql = "SELECT id, name FROM exhibits";
+
+        try (var ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Object idField = rs.getObject("id");
+                Object nameField = rs.getObject("name");
+                if (idField instanceof Integer) {
+                    int id = (Integer) idField;
+                    System.out.println(id);
+                }
+                if (nameField instanceof String) {
+                    String name = (String) nameField;
+                    System.out.println(name);
+                }
+            }
+        }
+    }
+}
