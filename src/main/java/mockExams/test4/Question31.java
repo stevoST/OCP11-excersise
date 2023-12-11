9d87147be6d868b53461f5ba4f984918ccc1a5b4
Stefan Starosta <stefan.starosta@gmail.com>
2023-12-11 21:34:13 +0100

    test4 question31

---
+++ src/main/java/mockExams/test4/Question31.java
@@ -0,0 +1,14 @@
+package mockExams.test4;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Question31 {
+    public static void main(String[] args) {
+        List<String> al = Arrays.asList("aa", "aaa", "b", "cc", "ccc", "ddd", "a");
+        //INSERT CODE HERE
+//        long count = al.stream().filter((str)->str.compareTo("c")<0).count();
+        long count = al.stream().filter((str)->str.compareTo("c")<0).sorted().count();
+        System.out.println(count);
+    }
+}
